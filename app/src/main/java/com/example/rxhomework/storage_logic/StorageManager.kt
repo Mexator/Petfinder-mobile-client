40907580d452399d459877d3c81100aa3d0bcafe
Anton Brisilin <a.brisilin@innopolis.university>
2020-05-07 23:37:43 +0300

    make storage manager singletone

--- app/src/main/java/com/example/rxhomework/storage_logic/StorageManager.kt
+++ app/src/main/java/com/example/rxhomework/storage_logic/StorageManager.kt
@@ -3,6 +3,7 @@ package com.example.rxhomework.storage_logic
 import android.content.Context
 import android.content.SharedPreferences
 import com.example.rxhomework.R
+import com.example.rxhomework.basic_logic.SingletonHolder
 
 class StorageManager(context: Context){
     private var tokensPrefs: SharedPreferences
@@ -10,4 +11,5 @@ class StorageManager(context: Context){
         val filename = context.resources.getString(R.string.tokens_file_name)
         tokensPrefs = context.getSharedPreferences(filename, Context.MODE_PRIVATE)
     }
+    companion object: SingletonHolder<StorageManager, Context>(::StorageManager)
 }
