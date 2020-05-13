18cf91a6e9c29dd17cf7543ad6cb8a940578984c
Anton Brisilin <a.brisilin@innopolis.university>
2020-05-13 23:06:59 +0300

    removed comments

--- app/src/main/java/com/example/rxhomework/network/api_interaction/APIKeysHolder.kt
+++ app/src/main/java/com/example/rxhomework/network/api_interaction/APIKeysHolder.kt
@@ -34,29 +34,13 @@ object APIKeysHolder{
         else true
     }
 
-    /**
-     * The function returns Single parameterized with String. The API access token can be obtained
-     * from it.
-     * Single -- Observable from RxJava library, that emits one value and then finishes.
-     * http://reactivex.io/documentation/single.html
-     * Just like Observable, you can subscribe on it by calling subscribe() method. Two lambdas
-     * should be passed there: OnSuccess action, and OnError action (second can be omitted, but it is
-     * a bad practise to do so). Single has no OnNext handler, and its only value can be obtained with
-     * OnSuccess.
-     * So, if you want to get the key - just subscribe on it. Probably, you want to do it in
-     * separate thread, then you could use subscribeOn and ObserveOn
-     * If you want to use in other reactive stream, check this link
-     * https://openclassrooms.com/en/courses/4788266-integrate-remote-data-into-your-app/5293916-chaining-different-network-queries-with-rxjava
-     */
     fun getAccessToken(): Single<String> {
         if (isTokenUpdateNeeded()) {
-            //TODO: DISPOSABLE PRODUCE MEMORY LEAK!!!
             val apiCall =
                 NetworkService
                     .petfinderAPI
                     .getAuthToken(api_key = apiKey, api_secret = apiSecret)
 
-            // Subscribe here to update class members and save to dis
             val disposable = apiCall
                 .subscribe { v: JsonObject ->
                     run {
