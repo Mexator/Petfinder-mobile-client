156b1db34849e5da12fca743f4e98ff29ee350de
Anton Brisilin <a.brisilin@innopolis.university>
2020-06-30 14:58:54 +0300

    minor changes

--- app/src/main/java/com/example/rxhomework/network/NetworkService.kt
+++ app/src/main/java/com/example/rxhomework/network/NetworkService.kt
@@ -1,5 +1,6 @@
 package com.example.rxhomework.network
 
+import com.example.rxhomework.ApplicationController
 import com.example.rxhomework.network.api_interaction.PetfinderJSONAPI
 import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
@@ -18,20 +19,21 @@ object NetworkService {
 
     val petfinderAPI: PetfinderJSONAPI
     private var mRetrofit: Retrofit
-    private var queryLogger: HttpLoggingInterceptor = HttpLoggingInterceptor()
 
     init {
-        queryLogger.level = HttpLoggingInterceptor.Level.BODY
-
         val client = OkHttpClient.Builder()
-        client.addInterceptor(queryLogger)
+            .addInterceptor(
+                HttpLoggingInterceptor()
+                    .setLevel(HttpLoggingInterceptor.Level.BODY)
+            )
 
         mRetrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
             .client(client.build())
-            .addConverterFactory(GsonConverterFactory.create())
             .addCallAdapterFactory(RxJava2CallAdapterFactory.createAsync())
+            .addConverterFactory(GsonConverterFactory.create())
             .build()
+
         petfinderAPI = mRetrofit.create(PetfinderJSONAPI::class.java)
     }
 
@@ -39,7 +41,7 @@ object NetworkService {
         // Just check whether we can ping google (our API endpoint does not respond to ping requests)
         val command = "ping -c 1 -W $TIMEOUT $INTERNET_CHECK_URL";
         return Single
-            .defer{Single.just(Runtime.getRuntime().exec(command).waitFor())}
+            .defer { Single.just(Runtime.getRuntime().exec(command).waitFor()) }
             .subscribeOn(Schedulers.io())
             // Trick to map an error to just a false value
             .onErrorReturnItem(-1)
