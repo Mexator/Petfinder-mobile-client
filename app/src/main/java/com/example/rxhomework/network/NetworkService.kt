2555ed030f4b83de4dc5b066a4c92e23f4e4cff8
Anton Brisilin <a.brisilin@innopolis.university>
2020-06-30 18:18:52 +0300

    Fixed html echaracters escaping

--- app/src/main/java/com/example/rxhomework/network/NetworkService.kt
+++ app/src/main/java/com/example/rxhomework/network/NetworkService.kt
@@ -2,6 +2,8 @@ package com.example.rxhomework.network
 
 import com.example.rxhomework.ApplicationController
 import com.example.rxhomework.network.api_interaction.PetfinderJSONAPI
+import com.example.rxhomework.utils.HtmlDeserializer
+import com.google.gson.GsonBuilder
 import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
 import okhttp3.OkHttpClient
@@ -27,11 +29,15 @@ object NetworkService {
                     .setLevel(HttpLoggingInterceptor.Level.BODY)
             )
 
+        val gson = GsonBuilder()
+            .registerTypeAdapter(String::class.java, HtmlDeserializer())
+            .create()
+
         mRetrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
             .client(client.build())
             .addCallAdapterFactory(RxJava2CallAdapterFactory.createAsync())
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
 
         petfinderAPI = mRetrofit.create(PetfinderJSONAPI::class.java)
---
+++ app/src/main/java/com/example/rxhomework/utils/HtmlDeserializer.kt
@@ -0,0 +1,20 @@
+package com.example.rxhomework.utils
+
+import com.google.gson.JsonDeserializationContext
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
+import com.google.gson.JsonParseException
+import org.apache.commons.lang3.StringEscapeUtils
+import java.lang.reflect.Type
+
+
+class HtmlDeserializer: JsonDeserializer<String> {
+    @Throws(JsonParseException::class)
+    override fun deserialize(
+        json: JsonElement, typeOfT: Type?,
+        context: JsonDeserializationContext?
+    ): String? {
+        val intermediate = StringEscapeUtils.unescapeHtml4(json.asString)
+        return StringEscapeUtils.unescapeHtml4(intermediate)
+    }
+}
