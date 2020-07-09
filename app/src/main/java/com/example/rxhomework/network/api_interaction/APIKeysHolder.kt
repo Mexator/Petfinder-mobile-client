b87fa2d7bcb4436d9fa394f28b2338031cc47aef
Anton Brisilin <a.brisilin@innopolis.university>
2020-07-09 20:50:02 +0300

    Moved saving/loading out of AKH

--- app/src/main/java/com/example/rxhomework/network/api_interaction/APIKeysHolder.kt
+++ app/src/main/java/com/example/rxhomework/network/api_interaction/APIKeysHolder.kt
@@ -8,7 +8,6 @@ import io.reactivex.Single
 import org.koin.core.KoinComponent
 import org.koin.core.inject
 import retrofit2.HttpException
-import java.text.ParseException
 import java.util.*
 
 class APIKeysHolder(
@@ -25,7 +24,11 @@ class APIKeysHolder(
     private val millisToSecs = 1e-3
 
     init {
-        loadFromPreferences()
+        with(storageManager.loadAPIKeys()) {
+            accessToken = first
+            initializedIn = second
+            expirationTime = third
+        }
     }
 
     private fun isInitialized(): Boolean {
@@ -63,44 +66,6 @@ class APIKeysHolder(
         this.accessToken = data.access_token
         this.expirationTime = data.expires_in
         this.initializedIn = Date()
-        this.saveToPreferences()
-    }
-
-    private fun saveToPreferences() {
-        // Save only of initialized. We don't need to store default nulls
-        if (isInitialized()) {
-            with(
-                storageManager
-                    .tokensPreferences
-                    .edit()
-            ) {
-                clear()
-                putString("access_token", accessToken)
-                putString(
-                    "initialized_in", storageManager
-                        .defaultDateTimeFormat
-                        .format(initializedIn!!)
-                )
-                putInt("expires_in", expirationTime)
-                apply()
-            }
-        }
-    }
-
-    private fun loadFromPreferences() {
-        val prefs = storageManager.tokensPreferences
-
-        this.accessToken = prefs.getString("access_token", null)
-        val rawDate = prefs.getString("initialized_in", "")
-
-        try {
-            this.initializedIn = storageManager
-                .defaultDateTimeFormat
-                .parse(rawDate!!)
-        } catch (ex: ParseException) {
-            this.initializedIn = null
-        }
-
-        this.expirationTime = prefs.getInt("expires_in", -1)
+        storageManager.saveAPIKeys(accessToken!!, initializedIn!!, expirationTime)
     }
 }
--- app/src/main/java/com/example/rxhomework/storage/StorageManager.kt
+++ app/src/main/java/com/example/rxhomework/storage/StorageManager.kt
@@ -2,7 +2,6 @@ package com.example.rxhomework.storage
 
 import android.content.Context
 import android.content.SharedPreferences
-import com.example.rxhomework.ApplicationController
 import com.example.rxhomework.R
 import org.koin.core.KoinComponent
 import org.koin.core.inject
@@ -10,15 +9,36 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 object StorageManager : KoinComponent {
-    var tokensPreferences: SharedPreferences
-        private set
-    var defaultDateTimeFormat: SimpleDateFormat
-        private set
+    private var tokensPreferences: SharedPreferences
+    private var defaultDateTimeFormat: SimpleDateFormat
     private val appContext: Context by inject()
 
     init {
         val filename = appContext.resources.getString(R.string.tokens_file_name)
         tokensPreferences = appContext.getSharedPreferences(filename, Context.MODE_PRIVATE)
         defaultDateTimeFormat = SimpleDateFormat("dd-MM-yyyy HH:mm:ss", Locale.US)
+    }
+
+    fun saveAPIKeys(token: String, initializedIn: Date, expiresIn: Int) {
+        with(tokensPreferences.edit()) {
+            clear()
+            putString("access_token", token)
+            putString("initialized_in", defaultDateTimeFormat.format(initializedIn))
+            putInt("expires_in", expiresIn)
+            apply()
+        }
+    }
+
+    fun loadAPIKeys(): Triple<String?, Date?, Int> {
+        val accessToken = tokensPreferences.getString("access_token", null)
+        val rawDate = tokensPreferences.getString("initialized_in", null)
+
+        val date = rawDate?.let {
+            defaultDateTimeFormat
+                .parse(it)
+        }
+
+        val expirationTime = tokensPreferences.getInt("expires_in", -1)
+        return Triple(accessToken, date, expirationTime)
     }
 }
