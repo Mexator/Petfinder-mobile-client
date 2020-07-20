345792bcbc901a7337a54c8215ed4957366e5bd6
Anton Brisilin <a.brisilin@innopolis.university>
2020-07-20 15:05:28 +0300

    Added dummy navigation drawer

--- app/src/main/java/com/mexator/petfinder_client/ui/activity/MainActivity.kt
+++ app/src/main/java/com/mexator/petfinder_client/ui/activity/MainActivity.kt
@@ -2,12 +2,21 @@ package com.mexator.petfinder_client.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import com.mexator.petfinder_client.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(drawer)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else
+            super.onBackPressed()
     }
 }
