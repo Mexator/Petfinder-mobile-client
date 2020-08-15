f7aafe8b5db27c9aaf7269008d48ae4eb3a51c8c
Anton Brisilin <a.brisilin@innopolis.university>
2020-08-15 15:25:16 +0300

    Dummy ViewModel and View

--- app/src/main/java/com/mexator/petfinder_client/mvvm/viewmodel/liked_pets/LikedPetViewModel.kt
+++ app/src/main/java/com/mexator/petfinder_client/mvvm/viewmodel/liked_pets/LikedPetViewModel.kt
@@ -1,24 +1,21 @@
 package com.mexator.petfinder_client.mvvm.viewmodel.liked_pets
 
 import androidx.lifecycle.ViewModel
+import com.mexator.petfinder_client.mvvm.viewstate.LikedPetsViewState
 import com.mexator.petfinder_client.mvvm.viewstate.MainViewState
 import io.reactivex.Observable
 import io.reactivex.subjects.BehaviorSubject
 import org.koin.core.KoinComponent
 
 class LikedPetViewModel : ViewModel(), KoinComponent {
-    private val _viewState: BehaviorSubject<MainViewState> = BehaviorSubject.create()
-    val viewState: Observable<MainViewState>
+    private val _viewState: BehaviorSubject<LikedPetsViewState> = BehaviorSubject.create()
+    val viewState: Observable<LikedPetsViewState>
         get() = _viewState
 
     init {
         _viewState.onNext(
-            MainViewState(
-                updating = false,
-                petList = emptyList(),
-                requestBreed = null,
-                requestType = null,
-                error = null
+            LikedPetsViewState(
+                emptyList()
             )
         )
     }
@@ -26,11 +23,4 @@ class LikedPetViewModel : ViewModel(), KoinComponent {
     fun loadNextPage() {
         TODO("Not yet implemented")
     }
-
-    fun reloadPetsList(type: String?, breed: String?) {
-        TODO("Not yet implemented")
-    }
-
-    val refreshNeeded: Boolean
-        get() = TODO("Not yet implemented")
 }
--- app/src/main/java/com/mexator/petfinder_client/ui/fragment/liked_pets/LikedPetsFragment.kt
+++ app/src/main/java/com/mexator/petfinder_client/ui/fragment/liked_pets/LikedPetsFragment.kt
@@ -5,14 +5,49 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.mexator.petfinder_client.R
+import com.mexator.petfinder_client.mvvm.viewmodel.liked_pets.LikedPetViewModel
+import com.mexator.petfinder_client.mvvm.viewstate.LikedPetsViewState
+import com.mexator.petfinder_client.ui.fragment.pet_search.list.PetAdapter
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 
 class LikedPetsFragment : Fragment() {
+    private val viewModel: LikedPetViewModel by viewModels()
+    private val compositeDisposable = CompositeDisposable()
+    private val adapter: PetAdapter = PetAdapter { }
+
+    init {
+        adapter.setHasStableIds(true)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return layoutInflater.inflate(R.layout.fragment_liked, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        subscribeToViewState()
+    }
+
+    override fun onDestroy() {
+        compositeDisposable.clear()
+        super.onDestroy()
+    }
+
+    private fun subscribeToViewState() {
+        val job = viewModel.viewState
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { state -> applyStateChange(state) }
+        compositeDisposable.add(job)
+    }
+
+    private fun applyStateChange(state: LikedPetsViewState) {
+
     }
 }
