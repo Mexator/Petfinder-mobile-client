1d31febb4d227b94e148ee5c645d5db7bf382780
Anton Brisilin <a.brisilin@innopolis.university>
2020-11-29 20:26:42 +0300

    Update apache text library

--- app/src/main/java/com/mexator/petfinder_client/utils/HtmlDeserializer.kt
+++ app/src/main/java/com/mexator/petfinder_client/utils/HtmlDeserializer.kt
@@ -4,10 +4,14 @@ import com.google.gson.JsonDeserializationContext
 import com.google.gson.JsonDeserializer
 import com.google.gson.JsonElement
 import com.google.gson.JsonParseException
-import org.apache.commons.lang3.StringEscapeUtils
+import org.apache.commons.text.StringEscapeUtils
 import java.lang.reflect.Type
 
-
+/**
+ * [HtmlDeserializer] is used to deserialize JSON that was urlencoded twice.
+ * Since it is impossible to change backend, this class was created to
+ * workaround this issue
+ */
 class HtmlDeserializer: JsonDeserializer<String> {
     @Throws(JsonParseException::class)
     override fun deserialize(
